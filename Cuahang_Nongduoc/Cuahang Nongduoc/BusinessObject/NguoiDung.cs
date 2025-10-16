c55154d9cc8006882683aa981a285e45ea79f97e
Mai Huy <61221969+huymai975@users.noreply.github.com>
2025-10-16 21:30:00 +0700

    Add files via upload

---
+++ Cuahang_Nongduoc/Cuahang Nongduoc/BusinessObject/NguoiDung.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CuahangNongduoc.BusinessObject
+{
+    public class NguoiDung
+    {
+        public int UserID { get; set; }
+        public string TenDangNhap { get; set; }
+        public string MatKhauHash { get; set; }
+        public string VaiTro { get; set; }
+        public string TenNguoiDung { get; set; }
+        public string Email { get; set; }
+        public string SoDienThoai { get; set; }
+        public bool TrangThai { get; set; }
+    }
+}
